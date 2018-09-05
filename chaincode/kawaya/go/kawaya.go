1b7b7e7fa9ac83a30c669cb13f08756778e262c6
AltTether <200351p@ugs.kochi-tech.ac.jp>
2018-09-05 15:23:07 +0900

    [add]init・invoke関数を作成

--- chaincode/kawaya/go/kawaya.go
+++ chaincode/kawaya/go/kawaya.go
@@ -1,8 +1,29 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+
+	"github.com/hyperledger/fabric/core/chaincode/shim"
+	sc "github.com/hyperledger/fabric/protos/peer"
+)
+
+
+type SmartContract struct {
+}
+
 type User struct {
   Id              string `json:"id"`
   Password        string `json:"password"`
   Balance         int    `json:"balance"`
   ReservedRoomId  string `json:"reserved_room_id"`
+}
+
+const DateTimeFormat = "2006-01-02 15:04:05 UTC"
+type Room struct {
+  Id              string    `json:"id"`
+  StatusOfUse     string    `json:"status_of_use"`
+  UnreservingTime time.Time `json:"unreserving_time"`
 }
 
 type Status int
@@ -17,9 +38,20 @@ type ResultUser struct {
   User    User    `json:"user"`
 }
 
-const DateTimeFormat = "2006-01-02 15:04:05 UTC"
-type Room struct {
-  Id              string    `json:"id"`
-  StatusOfUse     string    `json:"status_of_use"`
-  UnreservingTime time.Time `json:"unreserving_time"`
+func (s *SmartContract) Init(APIstub shim.ChaincodeStubInterface) sc.Response {
+	return shim.Success(nil)
+}
+
+func (s *SmartContract) Invoke(APIstub shim.ChaincodeStubInterface) sc.Response {
+
+	return shim.Error("Invalid Smart Contract function name.")
+}
+
+func main() {
+
+	// Create a new Smart Contract
+	err := shim.Start(new(SmartContract))
+	if err != nil {
+		fmt.Printf("Error creating new Smart Contract: %s", err)
+	}
 }
