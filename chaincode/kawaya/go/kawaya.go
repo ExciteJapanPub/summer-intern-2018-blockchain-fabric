edadff454ffc48ed6012eaa26554c09fc3d0cc63
AltTether <200351p@ugs.kochi-tech.ac.jp>
2018-09-05 15:23:07 +0900

    [add]init・invoke関数を作成

--- chaincode/kawaya/go/kawaya.go
+++ chaincode/kawaya/go/kawaya.go
@@ -1,3 +1,17 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+
+	"github.com/hyperledger/fabric/core/chaincode/shim"
+	sc "github.com/hyperledger/fabric/protos/peer"
+)
+
+
+type SmartContract struct {
+}
+
 type User struct {
   Id              string `json:"id"`
   Password        string `json:"password"`
@@ -15,4 +29,22 @@ const (
 type ResultUser struct {
   Status  Status  `json:"status"`
   User    User    `json:"user"`
+}
+
+func (s *SmartContract) Init(APIstub shim.ChaincodeStubInterface) sc.Response {
+	return shim.Success(nil)
+}
+
+func (s *SmartContract) Invoke(APIstub shim.ChaincodeStubInterface) sc.Response {
+
+	return shim.Error("Invalid Smart Contract function name.")
+}
+
+func main() {
+
+	// Create a new Smart Contract
+	err := shim.Start(new(SmartContract))
+	if err != nil {
+		fmt.Printf("Error creating new Smart Contract: %s", err)
+	}
 }
